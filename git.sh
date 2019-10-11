57325ce2d22830a267f2e1bd0b4a57578fb56918
Rasoul Azizi <rasoul.azizi@gmail.com>
2019-10-12 02:58:31 +0330

    tf gpu

--- git.sh
+++ git.sh
@@ -8,5 +8,5 @@ git checkout master
 git merge tmp
 git branch -d tmp
 git commit -m "new commit"
-git remote set-url origin https://ineuron1979:*******@github.com/ineuron1979/Bio_p001.git
+git remote set-url origin https://github.com/ineuron1979/Bio_p001.git
 git push origin master
