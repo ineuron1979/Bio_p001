0394d0d3586edc4a1d53ce91220696b0cb5ab216
Rasoul Azizi <rasoul.azizi@gmail.com>
2019-10-12 03:13:13 +0330

    new

--- git.sh
+++ git.sh
@@ -8,5 +8,5 @@ git checkout master
 git merge tmp
 git branch -d tmp
 git commit -m "new commit"
-git remote set-url origin https://ineuron1979@github.com/ineuron1979/Bio_p001.git
+git remote set-url origin https://ineuron1979:bd0bb82018702d895d5ecd99d0476961eff26ace@github.com/ineuron1979/Bio_p001.git
 git push origin master
