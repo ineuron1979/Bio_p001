730fa62eaa30a017f075f723f113e738eac2e71a
ineuron1979 <56010086+ineuron1979@users.noreply.github.com>
2019-10-12 01:19:37 +0330

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,8 +1,12 @@
-it add .
-    git branch tmp
-    git checkout master
-    git merge tmp
-    git branch -d tmp
-    git commit -m "new commit"
-    git remote set-url origin https://ineuron1979:7186fa52b7a63fb2fe6f2b29d34db0ccf1ceb816@github.com/ineuron1979/Bio_p001/
-    git push origin master
+
+git config --global user.email "rasoul.azizi@gmain.com"
+git config --global user.name "Rasoul Azizi"
+
+git add .
+git branch tmp
+git checkout master
+git merge tmp
+git branch -d tmp
+git commit -m "new commit"
+git remote set-url origin https://ineuron1979:7186fa52b7a63fb2fe6f2b29d34db0ccf1ceb816@github.com/ineuron1979/Bio_p001/
+git push origin master
